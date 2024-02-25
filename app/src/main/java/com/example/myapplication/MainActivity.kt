c4509e65108bc041dda938fd53bd21836987de4e
IohannesFolbort <petrarce@gmail.com>
2024-02-25 19:05:28 +0100

    optimize includes + minor padding updates

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,8 +14,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -27,27 +28,22 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import java.lang.NumberFormatException
 import java.text.NumberFormat
-import kotlin.math.roundToInt
-import androidx.compose.material3.Icon
-import androidx.compose.ui.res.painterResource
 
 
 class MainActivity : ComponentActivity() {
@@ -98,8 +94,10 @@ fun SwitchWithLabel(
 fun TimCalculator(modifier: Modifier = Modifier.fillMaxSize(), app: MainActivity? = null) {
     Column(
         modifier = modifier
+            .statusBarsPadding()
             .padding(16.dp)
             .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
         , verticalArrangement = Arrangement.Center
         , horizontalAlignment = Alignment.CenterHorizontally
     ) {
